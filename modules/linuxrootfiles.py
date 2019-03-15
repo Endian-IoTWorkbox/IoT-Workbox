41ec59bd8f245c440ab25c0ecf894d24f25eaa9d
Ctrl-cCtrl-xCtrl-v <44229840+Ctrl-cCtrl-xCtrl-v@users.noreply.github.com>
2019-03-15 13:53:59 +0000

    Updated linuxrootfiles.py
    The module now extends the Module BaseClass

--- modules/linuxrootfiles.py
+++ modules/linuxrootfiles.py
@@ -4,17 +4,17 @@ import re
 import os
 from helpers.colours import info, plus, minus, warning
 import stat
-
-try:
-    from tabulate import tabulate
-except ImportError:
-    minus("Please make sure all required libraries are installed, pip3 install -r requirements.txt")
+import datetime
+from modules.module import Module 
 
 
-class LinuxRootFiles(object):
+class LinuxRootFiles(Module):
 
     def __init__(self, *args, **kwargs):
-        
+        super().__init__(*args, **kwargs)
+
+        self.log_file = args[0]
+
         self.info = {
                 'Name': 'Linux Root files',
                 'Author': 'Alessandro Cara @ctrl_c3',
@@ -43,46 +43,43 @@ class LinuxRootFiles(object):
 
         if self.options['Verbose']['Value'] == "True":
             info("Checking file permissions for /etc/passwd")
-        
+            self.log_to_file("Checking file permissions for /etc/passwd")
+
         p = oct(stat.S_IMODE(os.stat('/etc/passwd').st_mode))[2:]
         info("/etc/passwd = %s" % p)
+        self.log_to_file("/etc/passwd = " + p)
         
         if int(p) != 644:
             minus("/etc/passwd permissions should be 644 (-rw-r--r--)")
+            self.log_to_file("/etc/passwd permissions shuld be 644 (-rw-r--r--)")
             error = 1 
+
         if self.options['Verbose']['Value'] == "True":
             info("Checking file permissions for /etc/shadow")
+            self.log_to_file("Checking file permissions for /etc/shadow")
         
         s = oct(stat.S_IMODE(os.stat('/etc/shadow').st_mode))[2:]
         info("/etc/shadow = %s " % s)
+        self.log_to_file("/etc/shadow = " + str(s))
         
         if int(s) != 640:
             minus("/etc/shadow permissions should be 640 (-rw-r-----)")
+            self.log_to_file("/etc/shadow permissions shuold be 640 (-rw-r-----)")
             error = 1
+
         if self.options['Verbose']['Value'] == "True":
             info("Checking file permissions for /etc/group")
+            self.log_to_file("Checking file permissions for /etc/group")
 
         g = oct(stat.S_IMODE(os.stat('/etc/group').st_mode))[2:]
         info("/etc/group = %s" % g)
+        self.log_to_file("/etc/group = " + str(g))
 
         if int(g) != 644:
             minus("/etc/group permissions should be 640 (-rw-r--r--)")
+            self.log_to_file("/etc/group permissions should be 640 (-rw-r--r--)")
             error = 1
 
         if error != 1:
             plus("Everything seems fine")
-    def print_options(self):
-        table = []
-        for key, value in self.options.items():
-
-            table.append([key, value['Description'], value['Value'], value['Required']])
-
-        print(tabulate(table, headers=["Option", "Description", "Value", "Required"], tablefmt="grid"))
-
-    def print_info(self):
-        table = []
-        for key, value in self.info.items():
-            table.append([key, value])
-
-        print(tabulate(table, headers=["Info", "Value"], tablefmt="grid"))
-
+            self.log_to_file("Everything seems fine")
